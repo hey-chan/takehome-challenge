a97fa611bb5947b973802bf3c873156849d02c7a
hey-chan <liumatthew8@gmail.com>
2022-03-02 22:21:04 +1100

    challenge complete

--- ruby/tasks/4-longest.rb
+++ ruby/tasks/4-longest.rb
@@ -1,10 +1,11 @@
 #! rspec ./../tests/4-longest.rb
 
 #
- # Returns the longest string out of an array of strings.
- # @param {Array} strings An array that might contain strings.
- # @returns {String} The longest string from within the input array.
+# Returns the longest string out of an array of strings.
+# @param {Array} strings An array that might contain strings.
+# @returns {String} The longest string from within the input array.
 #
 def longest_string(strings)
-
+  only_strings_allowed = strings.delete_if { |value| !(value.is_a? String) }
+  only_strings_allowed.max_by(&:length)
 end
