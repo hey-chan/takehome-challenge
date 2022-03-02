d6d6003ccf4df2ef8d85af2e46fa405c5b3d75c6
hey-chan <liumatthew8@gmail.com>
2022-03-02 22:12:38 +1100

    5th challenge complete

--- ruby/tasks/5-array-sum.rb
+++ ruby/tasks/5-array-sum.rb
@@ -1,11 +1,19 @@
 #! rspec ./../tests/5-array-sum.rb
 
 #
- # Sums all integers in a nested array, no matter how many levels deep.
- # @param {Array} startOfTree An array containing other arrays, ints, strings..
- # @returns {Number} The sum of all integers contained in the input, at any level.
+# Sums all integers in a nested array, no matter how many levels deep.
+# @param {Array} startOfTree An array containing other arrays, ints, strings..
+# @returns {Number} The sum of all integers contained in the input, at any level.
 #
 def array_sum(start_of_tree)
-
+  flat_array = start_of_tree.flatten
+  only_integers = []
+  for i in flat_array
+    if i.is_a? Integer
+      only_integers.push(i)
+    end
+  end
+  return only_integers.sum
 end
 
+puts array_sum(["Hi", 1, [[[[[[[[[2]]], 3]]]]]]])
