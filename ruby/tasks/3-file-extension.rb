898008b169da49f0794caa4122f2c034348ae5b6
hey-chan <liumatthew8@gmail.com>
2022-03-02 21:59:58 +1100

    3rd challenge complete

--- ruby/tasks/3-file-extension.rb
+++ ruby/tasks/3-file-extension.rb
@@ -1,10 +1,16 @@
 #! rspec ./../tests/3-file-extension.rb
 
 #
- # Returns the file extension of a filename.
- # @param {String} filename The string from which to extract the file extension
- # @returns {String} The file extension (with no period), or false if there is none.
+# Returns the file extension of a filename.
+# @param {String} filename The string from which to extract the file extension
+# @returns {String} The file extension (with no period), or false if there is none.
 #
 def get_file_extension(filename)
-
+  if !filename.include? "."
+    return false
+  else
+    return filename.split(".")[-1]
+  end
 end
+
+puts get_file_extension("hello.htaccess")
